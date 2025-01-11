74f6bcb436dd5e37245d73d5a885290596761aa5
Charles Channon <channon.charles@gmail.com>
2025-01-11 09:32:41 -0500

    Unpackaged Deployment

--- obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SolutionManager.AssemblyInfo.cs
+++ obj/x64/Debug/net8.0-windows10.0.19041.0/win-x64/SolutionManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SolutionManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af8f3001b3aaf37a18346704dd0ef99dc21622ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("SolutionManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SolutionManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
